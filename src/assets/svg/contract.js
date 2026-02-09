62302d7f4af5a9d61e0c77ee7fb2e1c5a2265f3d
misam199696 <misamaliraza94@gmail.com>
2026-02-09 10:51:33 +0500

    Bottom Tabbar Updated

--- src/assets/svg/contract.js
+++ src/assets/svg/contract.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Svg, { Path } from "react-native-svg";
-const Contract = (props) => (
+const Contract = ({ stroke = "currentColor", strokeWidth = 1.66667, ...props }) => (
   <Svg
     width={20}
     height={20}
@@ -11,36 +11,36 @@ const Contract = (props) => (
   >
     <Path
       d="M6.66699 16.6673V6.66732C6.66699 4.83398 8.16699 3.33398 10.0003 3.33398C11.2503 3.33398 12.3337 4.00065 12.917 5.00065"
-      stroke="currentColor"
-      strokeWidth={1.66667}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <Path
       d="M5 10H8.33333"
-      stroke="currentColor"
-      strokeWidth={1.66667}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <Path
       d="M11.667 10H13.3337V16.6667"
-      stroke="currentColor"
-      strokeWidth={1.66667}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <Path
       d="M5 16.666H8.33333"
-      stroke="currentColor"
-      strokeWidth={1.66667}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <Path
       d="M11.667 16.666H15.0003"
-      stroke="currentColor"
-      strokeWidth={1.66667}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
     />
